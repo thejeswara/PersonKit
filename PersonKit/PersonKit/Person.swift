d1b7b3b1ebf5811602fbf1a3b9c30ac5a549f832
Thejeswara Reddy <thejeswara.reddy@NIT-MAC-R48.local>
2021-04-03 22:45:48 +0530

    Initial commit

---
+++ PersonKit/PersonKit/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  PersonKit
+//
+//  Created by Thejeswara Reddy on 03/04/21.
+//
+
+import Foundation
+
+public class Person{
+    
+    public init(){}
+    
+    public func getValues() -> String
+    {
+        return getColor()
+    }
+    
+    private func getColor() -> String{
+        return "Hello"
+    }
+}
---
+++ PersonKit/PersonKit/PersonKit.h
@@ -0,0 +1,18 @@
+//
+//  PersonKit.h
+//  PersonKit
+//
+//  Created by Thejeswara Reddy on 03/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PersonKit.
+FOUNDATION_EXPORT double PersonKitVersionNumber;
+
+//! Project version string for PersonKit.
+FOUNDATION_EXPORT const unsigned char PersonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersonKit/PublicHeader.h>
+
+
